e831f28bf5ed8d00b1bf57730fce29027d023613
huynhchin <chuynh@zagmail.gonzaga.edu>
2017-04-18 22:37:26 -0700

    almost done UX except images and dialog box

---
+++ src/tablePanel.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javax.swing.*;
+
+public class tablePanel extends JPanel{
+
+    private JToggleButton dealer1;
+    private JToggleButton dealer2;
+    private JToggleButton dealer3;
+    private JToggleButton dealer4;
+    private JToggleButton dealer5;
+    private JToggleButton aiOne1;
+    private JToggleButton aiOne2;
+    private JToggleButton aiOne3;
+    private JToggleButton aiOne4;
+    private JToggleButton aiOne5;
+    private JToggleButton aiTwo1;
+    private JToggleButton aiTwo2;
+    private JToggleButton aiTwo3;
+    private JToggleButton aiTwo4;
+    private JToggleButton aiTwo5;
+    private JToggleButton aiThree1;
+    private JToggleButton aiThree2;
+    private JToggleButton aiThree3;
+    private JToggleButton aiThree4;
+    private JToggleButton aiThree5;
+    private JToggleButton player1;
+    private JToggleButton player2;
+    private JToggleButton player3;
+    private JToggleButton player4;
+    private JToggleButton player5;
+
+    public tablePanel(){
+    }
+}
